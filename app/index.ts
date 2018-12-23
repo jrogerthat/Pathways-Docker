b716c6419fea515e346c035ad974ab2b9bbf08d8
Jen <jen@sci.utah.edu>
2018-12-23 16:57:39 -0700

    first commit

---
+++ app/index.ts
@@ -0,0 +1,18 @@
+const app = require('express')();
+const http = require('http');
+const server = http.createServer(app);
+
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+
+
+app.set('port', (process.env.PORT || 3000));
+console.log('The port is:::: ', app.get('port'));
+
+server.listen(app.get('port'), () => {
+  console.log('---> listening on port ', app.get('port'));
+});
+
---
+++ dist/index.js
@@ -0,0 +1,12 @@
+const app = require('express')();
+const http = require('http');
+const server = http.createServer(app);
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+app.set('port', (process.env.PORT || 3000));
+console.log('The port is:::: ', app.get('port'));
+server.listen(app.get('port'), () => {
+    console.log('---> listening on port ', app.get('port'));
+});
+//# sourceMappingURL=index.js.map
